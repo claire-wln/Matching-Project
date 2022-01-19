bf739dedfd6de1887d0c1eb1e478f334882540ef
343466942 <343466942@C-2UA4041LJP.tdsb.on.ca>
2022-01-19 13:38:46 -0500

    update

--- 2020_Button_Advanced/src/hutchison/grant/MultiButton.java
+++ 2020_Button_Advanced/src/hutchison/grant/MultiButton.java
@@ -22,7 +22,7 @@ public class MultiButton extends Application {
 		launch(args);
 	}
 	
-	private final double WIDTH = 600;
+	private final double WIDTH = 800;
 	private final double HEIGHT = 600;
 	private final int NUM_ROWS = 6;
 	private final int NUM_COLS = 7;
