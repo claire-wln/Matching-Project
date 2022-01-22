896bbce0cc2e384bf1dca502bbc5dbb2dcf1f5e1
Claire <Claire@10.0.0.105>
2022-01-21 19:09:23 -0500

    implimented graphics

--- 2020_Button_Advanced/src/hutchison/grant/Driver.java
+++ 2020_Button_Advanced/src/hutchison/grant/Driver.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
@@ -13,11 +14,16 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Driver extends Application {
@@ -31,6 +37,7 @@ public class Driver extends Application {
 	private static final int NUM_ROWS = 4;
 	private static final int NUM_COLS = 5;
 	static int numCardsClicked = 0;
+	static int turnsTaken = 0;
 	ArrayList<NewButton> buttons = new ArrayList<>();
 
 	static NewButton[][] slots = new NewButton[NUM_ROWS][NUM_COLS];
@@ -41,13 +48,33 @@ public class Driver extends Application {
 	public void start(Stage primaryStage) throws Exception {
 
 		VBox main = new VBox();
-		// add title and score stuff
+		BackgroundFill bf = new BackgroundFill(Color.ORANGE,CornerRadii.EMPTY,Insets.EMPTY);
+		Background bg = new Background(bf);
+		main.setBackground(bg);
+
+		main.setAlignment(Pos.CENTER);
+		//title and score stuff
+		Text space = new Text("");
+		Text title = new Text("");
+		title.setFont(Font.font("Comic Sans MS", 30));
+		title.setFill(Color.WHITE);
+		title.setText("SUPER MEGA MATCHING CARD GAME EXTRAORDINAIRE PRO +");
+		main.getChildren().addAll(space, title);
+		//heyyyyyyyyyyyyyy
 		GridPane gridPane = new GridPane();
-		Scene mySceneGraph = new Scene(main, WIDTH + 100, HEIGHT + 100);
+		Scene mySceneGraph = new Scene(main,WIDTH+100,HEIGHT+100);
 		main.getChildren().add(gridPane);
 		gridPane.setAlignment(Pos.CENTER);
 
-		int turnCount = 0;
+		//SCORE STUFF
+		VBox score = new VBox();
+		main.setAlignment(Pos.BOTTOM_LEFT);
+
+		Text CurrentScore = new Text("Turns Taken: " );
+		CurrentScore.setFont(Font.font("Comic Sans MS", 29));
+		CurrentScore.setFill(Color.WHITE);
+		main.getChildren().add(CurrentScore);
+		
 		setBoard();
 
 		// setup slots as NewButton objects
@@ -62,29 +89,33 @@ public class Driver extends Application {
 						buttons.get(1).updateState(1);
 						buttons.clear();
 						numCardsClicked = 0;
-						System.out.println("reached numcards of 2");
+						//System.out.println("reached numcards of 2");
 						
 					}
 					refreshBoard();
-					System.out.println(((NewButton) e.getSource()).getState());
+					//System.out.println(((NewButton) e.getSource()).getState());
 					
 					if (((NewButton) e.getSource()).getState() == 1 && numCardsClicked == 0) {
 
 						((NewButton) e.getSource()).updateState(2);
-						System.out.println(numCardsClicked);
+						//System.out.println(numCardsClicked);
 						numCardsClicked++;
 						buttons.add((NewButton) e.getSource());
 
 					} else if (((NewButton) e.getSource()).getState() == 1 && numCardsClicked == 1) {
 						numCardsClicked++;
 						// check if match or not
-						System.out.println(numCardsClicked);
+						//System.out.println(numCardsClicked);
 						buttons.add((NewButton) e.getSource());
 						((NewButton) e.getSource()).updateState(2);
+						
+						//turn is over
+						turnsTaken ++;
+						CurrentScore.setText("Turns Taken: " + turnsTaken);
 
 						if (buttons.get(0).getName().equals(buttons.get(1).getName())) {
 
-							System.out.println("pair matched");
+							//System.out.println("pair matched");
 							// deactivate buttons
 							buttons.get(0).setDisable(true);
 							buttons.get(1).setDisable(true);
@@ -97,7 +128,7 @@ public class Driver extends Application {
 
 						} else { 
 							
-							System.out.println("NO matched");
+							//System.out.println("NO matched");
 						}
 						
 						refreshBoard();
@@ -153,7 +184,7 @@ public class Driver extends Application {
 				slots[i][j].setGraphic(back);
 				slots[i][j].updateState(1);
 				
-				System.out.println("state: "+ slots[i][j].getState() + " at " + slots[i][j]);
+				//System.out.println("state: "+ slots[i][j].getState() + " at " + slots[i][j]);
 				
 				imgVal++;
 			}
@@ -181,5 +212,9 @@ public class Driver extends Application {
 				}
 			}
 		}
+	}
+	
+	public void updateTurns(int turns){
+		
 	}
 }
